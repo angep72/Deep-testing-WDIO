6f963e144eb68ee46d44e5311abb9182afbde4dd
angep73 <p.umunyana@alustudent.com>
2025-01-30 12:35:02 +0200

    this tests runs on wdio 9.2.0

--- WdioTests/test/specs/login.spec.js
+++ WdioTests/test/specs/login.spec.js
@@ -1,9 +1,15 @@
-describe('Demo test for login',function (){
-    it ('Login TESt',async function (){
-        browser.url('https://the-internet.herokuapp.com/login');
+describe('Demo test for login', function() {
+    it('Login Test', async function() {
+        await browser.url('https://the-internet.herokuapp.com/login');
         await $('#username').setValue('tomsmith');
-        await $('#password').setValue('SuperSecretPassword!'); 
+        await $('#password').setValue('SuperSecretPassword!');
         await $('button[type="submit"]').click();
-        browser.pause(5000)  
-    })
-})
+        
+        const flashMessage = await $('#flash');
+        await expect(await flashMessage.getText()).toContain('You logged into a secure area!');
+        // Or alternatively:
+        // await expect(flashMessage).toHaveText(expect.stringContaining('You logged into a secure area!'));
+        
+        await browser.pause(5000);
+    });
+});
