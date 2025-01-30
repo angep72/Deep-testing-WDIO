87d70909147e99a59a5d77a29e8cca486c24347e
angep73 <p.umunyana@alustudent.com>
2025-01-30 09:53:30 +0200

    adding different comments in my test

--- WdioTests/test/specs/test1.spec.js
+++ WdioTests/test/specs/test1.spec.js
@@ -2,5 +2,6 @@ describe("Demo ",function(){
     it("should open the login page", async () => {
          browser.url("https://www.google.com/");
          await $('#APjFqb').setValue('webdriverio');
+        browser.keys('Enter');
     })
 })
