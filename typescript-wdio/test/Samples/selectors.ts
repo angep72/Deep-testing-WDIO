58d04c72414e471500d8bb7c1a81f2bd7deaaa66
angep73 <p.umunyana@alustudent.com>
2025-02-04 09:11:06 +0200

    adding new functionalities

---
+++ typescript-wdio/test/Samples/selectors.ts
@@ -0,0 +1,7 @@
+describe("Name of the", () => {
+    it("should ", async() => {
+        browser.url("https://qavbox.github.io/demo/")
+        browser.pause(5000);
+        await $("=SignUp Form").click()
+    })
+})
